6691bf95c4baad7bf738b91175527fd1340f36af
ephraim1002 <etmakurumidze@gmail.com>
2023-05-11 18:41:19 +0200

    python and java updates

---
+++ java_tutorials/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+/* The code below will print the words Hello World
+to the screen, and it is amazing */
+
+public class Main{
+    public static void main(String[] args){
+    System.out.println("hello, world")
+    }
+   
+} 
+
+//Playing around with the System.out.println function
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("Hello World!");
+	System.out.println("I am learning Java.");
+	System.out.println("It is awesome!");
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+  }
+}
+
+int myNum = 5;               // Integer (whole number) 4 bytes
+float myFloatNum = 5.99f;    // Floating point number 4 bytes
+char myLetter = 'D';         // Character 2 bytes
+boolean myBool = true;       // Boolean 1 bit
+String myText = "Hello";     // String
