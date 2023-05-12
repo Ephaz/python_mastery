d310ae7a3587d9775a631f03c8fc9d61cff5e841
ephraim1002 <etmakurumidze@gmail.com>
2023-05-12 15:07:04 +0200

    first program update

--- python_tutorials/assignment_2.py
+++ python_tutorials/assignment_2.py
@@ -1,30 +1,29 @@
 import math
 
 house_portion_down_payment = 0.25
+annual_salary = float(input("How much is your annual salary? > "))
+total_cost_of_house = float(input("How much is your dream home? > "))
+portion_of_salary_to_contribute = float(input("What percentage of your salary would you like to save? > " ))
+
+#interest on savings
+r = 0.04/12
+
+#x is the amount contributed every month
+x = (annual_salary/12) * portion_of_salary_to_contribute 
+
+#amount required to save or the down payment amount
+amount_required_to_save = house_portion_down_payment * total_cost_of_house
+
+print(amount_required_to_save)
+
+#the current savings are zero, therefore it starts here
 current_savings = 0
-r = 0.04/12    #interest on savings
-current_savings = r * current_savings
-annual_salary = float(input("How much is your annual salary?"))
-total_cost = float(input("How much is your dream home?"))
-portion_saved = float(input("What percentage of your salary would you like to save?"))/100
 
-x = annual_salary/12 
+number_of_months = 0
 
-amount_required_to_save = house_portion_down_payment * total_cost
+while current_savings < amount_required_to_save:
+    current_savings += x + current_savings * r
+    number_of_months += 1
 
-current_savings = (x + current_savings) * (1+r)
-
-print(current_savings)
-
-contributions = []
-
-print (contributions)
-
-while contributions < amount_required_to_save:
-    contributions += (x + current_savings) * (1+r)
-    print(contributions)
-   
-total_months = amount_required_to_save/(contributions)
-
-print(total_months)
-print(contributions)
+#print the number of monts required to save to afford the deposit        
+print(f"Number of months:{number_of_months}")
