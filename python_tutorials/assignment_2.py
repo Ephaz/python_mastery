6d3c1b8f49fe9b36ef22b24195bfeb7dc5567036
ephraim1002 <etmakurumidze@gmail.com>
2023-05-12 10:01:58 +0200

    assignment 2 update

--- python_tutorials/assignment_2.py
+++ python_tutorials/assignment_2.py
@@ -1,18 +1,30 @@
 import math
 
-portion_down_payment = 0.25
+house_portion_down_payment = 0.25
 current_savings = 0
-r = 0.04    #interest on savings
+r = 0.04/12    #interest on savings
 current_savings = r * current_savings
 annual_salary = float(input("How much is your annual salary?"))
 total_cost = float(input("How much is your dream home?"))
-portion_saved = float(input("What percentage of your salary would you like to save?"))
+portion_saved = float(input("What percentage of your salary would you like to save?"))/100
 
 x = annual_salary/12 
 
-current_savings = x + current_savings * r
+amount_required_to_save = house_portion_down_payment * total_cost
 
-amount_required_to_save = portion_down_payment * total_cost
+current_savings = (x + current_savings) * (1+r)
 
-print(amount_required_to_save/x)
+print(current_savings)
 
+contributions = []
+
+print (contributions)
+
+while contributions < amount_required_to_save:
+    contributions += (x + current_savings) * (1+r)
+    print(contributions)
+   
+total_months = amount_required_to_save/(contributions)
+
+print(total_months)
+print(contributions)
